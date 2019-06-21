f876e2b47f6047ed75fe74b93ad85eb8d85409df
Na Hyung Choi <schoinh@gmail.com>
2019-06-21 16:04:52 -0700

    display pizzas built and totals

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Business Logic --------------------
 
 var sizeCosts = {   // Base cost for a pizza depending on its size
-  small: 6,
-  medium: 9,
-  large: 12,
-  xLarge: 15
+  S: 6,
+  M: 9,
+  L: 12,
+  XL: 15
 }
 
 function Order() {
-  this.currentId = 0
+  this.currentId = 1
   this.pizzas = [];
   this.total = 0;
 }
@@ -31,19 +31,39 @@ function Pizza(crust, basicToppings, premiumToppings, size, cost) {
 }
 
 Pizza.prototype.determineCost = function () {
-  if (this.crust === "glutenFree") {    // Gluten-free crust costs $1.50 extra
+  if (this.crust === "gluten-ree") {    // Gluten-free crust costs $1.50 extra
     this.cost += 1.5;
   }
   this.cost += (1 * this.basicToppings.length + 2 * this.premiumToppings.length);    // Charges $1 for each basic and $2 for each premium topping
   this.cost += sizeCosts[this.size];
 }
 
+var addTax = function (cost) {
+  cost = cost + cost * 0.1;
+  return cost.toFixed(2);
+}
+
 // UI Logic --------------------------
 
 var inputBasicTpgs = []
 var inputPremiumTpgs = []
+var newPizzaDetails = "";
+
+Pizza.prototype.formatDetails = function () {
+  var formattedBasicTpgs = this.basicToppings.join(", ");
+  var formattedPremTpgs = this.premiumToppings.join(", ");
+
+  var line1 = "Pizza #" + this.id + " - $" + this.cost.toFixed(2);
+  var line2 = "<ul><li>Size: " + this.size + "</li>"
+  var line3 = "<li>Crust: " + this.crust + "</li>";
+  var line4 = "<li>Basic Toppings: " + formattedBasicTpgs + "</li>";
+  var line5 = "<li>Premium Toppings: " + formattedPremTpgs + "</li></ul>";
+
+  newPizzaDetails = line1 + line2 + line3 + line4 + line5;
+}
 
 var getBasicTpgs = function () {
+  inputBasicTpgs = []
   $("input:checkbox[name=basic-toppings]:checked").each(function () {
     var basicTpg = $(this).val();
     inputBasicTpgs.push(basicTpg);
@@ -51,6 +71,7 @@ var getBasicTpgs = function () {
 }
 
 var getPremiumTpgs = function () {
+  inputPremiumTpgs = []
   $("input:checkbox[name=premium-toppings]:checked").each(function () {
     var premiumTpg = $(this).val();
     inputPremiumTpgs.push(premiumTpg);
@@ -66,15 +87,38 @@ $(function() {
   })
   
 
-  $("form.build-pizza").submit(function(event) {
+  $("form.build-pizza").submit(function(event) {    // When "Add to Order" button is clicked...
     event.preventDefault();
 
-    var inputSize = $("#size").val();
+    var inputSize = $("#size").val();   // Saves input as variables
     var inputCrust = $("#crust").val();
     getBasicTpgs();
     getPremiumTpgs();
 
-    var newPizza = new Pizza(inputCrust, inputBasicTpgs, inputPremiumTpgs, inputSize);
-    newOrder.addPizza(newPizza);
+    if (inputSize === "none") {
+      $("#size-alert").fadeIn();
+    } else {
+      var newPizza = new Pizza(inputCrust, inputBasicTpgs, inputPremiumTpgs, inputSize);    // Adds new pizza to order
+      newOrder.addPizza(newPizza);
+      
+      newPizza.formatDetails();
+      $("#ordered-items").append(newPizzaDetails);
+
+      $("#order-total").text(" $" + (newOrder.total).toFixed(2));
+      $("#tax").text(" $" + (newOrder.total * 0.1).toFixed(2));
+      $("#final-total").text(" $" + addTax(newOrder.total));
+      $("#total-print, #submit-order").show();
+  
+      $("form.build-pizza")[0].reset();   // Clears the form
+      $("#size-alert").hide();
+    }
+  });
+
+  $("form#contact-info").submit(function(event) {
+    event.preventDefault();
+
+    var inputName = $("input#name").val();
+    $("#username").text(inputName);
+    $("#submitted").fadeIn();
   })
-})
+});
