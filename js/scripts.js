f659332cb85c6ced81b8fd1f8f0b6792f96229dd
Na Hyung Choi <schoinh@gmail.com>
2019-06-21 10:47:47 -0700

    refactor .determineCost() with sizeCosts object

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
 // Business Logic --------------------
 
+var sizeCosts = {
+  small: 5,
+  medium: 8,
+  large: 11,
+  xLarge: 14
+}
+
 function Order() {
   this.currentId = 0
   this.pizzas = [];
   this.total = 0;
 }
 
-function Pizza(toppings, size, cost) {
+Order.prototype.addPizza = function (pizza) {
+  pizza.id = this.currentId;
+  pizza.determineCost();
+  this.total += pizza.cost;
+  this.pizzas.push(pizza);
+  this.currentId += 1;
+}
+
+function Pizza(basicToppings, premiumToppings, size, cost) {
   this.id;
-  this.toppings = toppings;
+  this.basicToppings = basicToppings;
+  this.premiumToppings = premiumToppings;
   this.size = size;
   this.cost;
 }
 
-// function Size()
-
-Order.prototype.addPizza = function (pizza) {
-  this.pizzas.push(pizza);
-  pizza.id = this.currentId;
-  this.currentId += 1;
-  this.total += pizza.cost;
-}
-
-Pizza.prototype.determineCost = function (toppings, size) {
-  this.cost = 1.25 * this.toppings.length;
-  if (this.size === "small") {
-    this.cost += 5;
-  } else {
-    this.cost += 8;
-  }
+Pizza.prototype.determineCost = function () {
+  this.cost = 1 * this.basicToppings.length + 2 * this.premiumToppings.length;
+  this.cost += sizeCosts[this.size];
 }
 
 // UI Logic --------------------------
